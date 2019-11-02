a21277d9560340a70211173ae759f91f08bfcc4a
Mohammad Amin <mak12776@gmail.com>
2019-11-03 00:07:01 +0330

    remove main.c, add main.cmd

--- main.c
+++
@@ -1,6 +0,0 @@
-
-#include "scl/scl.h"
-
-int main()
-{
-}
--- make.cmd
+++ make.cmd
@@ -1,1 +1,11 @@
-@gcc main.c -o main
+@echo off
+
+if "%1"=="bm" goto make_bm
+
+gcc main.c -o main
+goto end
+
+:make_bm
+gcc -Iscl bm\bm.c -o bm
+
+:end
